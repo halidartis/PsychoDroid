351cd50b51f03aff055a44fc593eb33b7c1f620e
kerem durak <durakkerem42@gmail.com>
2015-06-24 00:00:42 +0300

    İlk commit

--- app/src/main/java/droid/psycho/com/psychodroid/MainActivity.java
+++ app/src/main/java/droid/psycho/com/psychodroid/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // al bakim
         setContentView(R.layout.activity_main);
     }
 
