762557e05b9e353d30ad21589452cac1ad79d126
kerem durak <durakkerem42@gmail.com>
2015-06-25 14:56:47 +0300

    Belirtilen bir aralıktaki SMS'leri çekip txt dosyasına kaydededen 'ReadSMS' classı yazıldı.

--- app/src/main/java/droid/psycho/com/psychodroid/MainActivity.java
+++ app/src/main/java/droid/psycho/com/psychodroid/MainActivity.java
@@ -2,17 +2,47 @@ package droid.psycho.com.psychodroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
+    ReadSMS readSMS;
+    FileInputStream fis;
+    String sms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // al bakim
         setContentView(R.layout.activity_main);
-    }
 
+        readSMS = new ReadSMS();
+        readSMS.getSMSMessages(getApplicationContext(), 24, "keremdurak");
+        Button button = (Button) findViewById(R.id.button);
+        final TextView textView = (TextView)findViewById(R.id.tv_sms);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e("AHA", readSMS.readFromFile(getApplicationContext()));
+                textView.setText(readSMS.readFromFile(getApplicationContext()));
+
+            }
+        });
+
+
+
+
+        }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -34,4 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
---
+++ app/src/main/java/droid/psycho/com/psychodroid/ReadSMS.java
@@ -0,0 +1,114 @@
+package droid.psycho.com.psychodroid;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+
+public class ReadSMS {
+    Boolean SMSReadFinished = false;
+
+    public ReadSMS() {
+
+        // public static final String INBOX = "content://sms/inbox";
+        // public static final String SENT = "content://sms/sent";
+        // public static final String DRAFT = "content://sms/draft";
+
+    }
+
+    public void getSMSMessages(Context context, int intervalHour, String FileName) {
+        String str = "";
+        long currentDateInMillis = System.currentTimeMillis();
+        long yesterdayInMillis = currentDateInMillis - intervalHour * 3600000;
+
+
+        Cursor cursor = context.getContentResolver().query(Uri.parse("content://sms/sent"), new String[]{"_id", "address", "date", "body"}, null, null, null);
+
+        cursor.moveToFirst();
+
+        str += "MessageBody: " + cursor.getString(3) + " Address: " + cursor.getString(1) + "\n"; //first message.
+        while (cursor.moveToNext()) { //check following messages.
+            String date = cursor.getString(cursor.getColumnIndex("date")); //mesajın alınma-gönderilme tarihi
+            Long timestamp = Long.parseLong(date);
+
+            if (timestamp > yesterdayInMillis) {
+                Log.e("AHA", "MESAJIN TARIHI: " + date);
+                String address = cursor.getString(1);
+                String body = cursor.getString(3);
+                Log.e("AHA", "body: " + body);
+                Log.e("AHA", "MessageBody: " + body + " Address: " + address);
+                str += "MessageBody: " + body + " Address: " + address + "\n";
+
+
+            } else {
+                Log.e("AHA","buraya giriyo mu?");
+                break;
+            }
+
+
+        }
+        writeToFile("keremdurak", str, context);
+
+
+    }
+
+
+
+
+
+
+    private void writeToFile(String filename, String data, Context context) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput(filename + ".txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        } catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+    protected  String readFromFile(Context context) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = context.openFileInput("keremdurak.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString;
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+
+}
+
+
+
